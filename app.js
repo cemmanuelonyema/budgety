8efd1e3c825b9dba8daf2b051745c4612fc3a96b
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-05-03 17:32:07 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Budget Data Controller - Responsible for data modifications
+let budgetController = (function () {})();
+
+//UI Controller - Responsible for ui modifications
+let uiController = (function () {})();
+
+//Global  App Controller
+let controller = (function () {})(budgetController, uiController);
