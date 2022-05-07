23004e1e94b108058690cbd50f366b227d2bdec7
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-05-07 17:58:36 +0100

    Implemented ability to render ui input on the screen

--- app.js
+++ app.js
@@ -63,6 +63,8 @@ let UIController = (function () {
     inputType: ".add__type",
     inputDescription: ".add__description",
     inputValue: ".add__value",
+    incomeContainer: ".income__list",
+    expenseContainer: ".expenses__list",
   };
 
   return {
@@ -70,19 +72,42 @@ let UIController = (function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // either income or expense
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value,
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value),
       };
     },
     getDOMstrings: function () {
       return DOMstrings;
     },
 
-    addListItem: function () {
+    addListItem: function (obj, type) {
+      let html, newHtml, element;
       //create html string placeholder text
       if (type === "income") {
+        element = DOMstrings.incomeContainer;
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div> </div>';
       }
       if (type === "expense") {
+        element = DOMstrings.expenseContainer;
+        html =
+          '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div><div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div><div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div> </div> </div>';
       }
+      //Replace the placeHolder text with some actual date
+      newHtml = html.replace("%id%", obj.id);
+      newHtml = newHtml.replace("%description%", obj.description);
+      newHtml = newHtml.replace("%value%", obj.value);
+
+      //insert html
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
+    },
+    clearField: function () {
+      const eleDescription = (document.querySelector(
+        DOMstrings.inputDescription
+      ).value = " ");
+
+      document.querySelector(DOMstrings.inputValue).value = " ";
+
+      eleDescription.focus();
     },
   };
 })();
@@ -102,16 +127,21 @@ let controller = (function (budgetCtrl, UICtrl) {
   const ctrlAddItem = function () {
     // As soon as the enter/ submit btn is clicked, Get the filled input data
     const input = UICtrl.getInput();
-    console.log(input);
-    // Add the item to the budget controller
-    const newItem = budgetCtrl.budgetAddItem(
-      input.type,
-      input.description,
-      input.value
-    );
-    // Render the item to the ui
-    // Calc the budget
-    //  Display the budget on the ui
+    if (input.description !== "" && !isNaN(input.value)) {
+      console.log(input);
+      // Add the item to the budget controller
+      const newItem = budgetCtrl.budgetAddItem(
+        input.type,
+        input.description,
+        input.value
+      );
+      // Render the item to the ui
+      UICtrl.addListItem(newItem, input.type);
+      //clear the field
+      UICtrl.clearField();
+      // Calc the budget
+      //  Display the budget on the ui
+    }
   };
 
   return {
